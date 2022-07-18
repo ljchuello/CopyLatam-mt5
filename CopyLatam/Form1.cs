32145269963006a214585a5199cbb14b5edb5a08
LJChuello <LJChuello@DESKTOP-4DC1O1F>
2022-07-18 07:33:40 -0500

    Agregar archivos de proyecto.

---
+++ CopyLatam/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using MtApi5;
+
+namespace CopyLatam
+{
+    public partial class Form1 : Form
+    {
+        static readonly EventWaitHandle _connnectionWaiter = new AutoResetEvent(false);
+        static readonly MtApi5Client _mtapi = new MtApi5Client();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Console.WriteLine("Application started.");
+
+            _mtapi.ConnectionStateChanged += _mtapi_ConnectionStateChanged;
+            _mtapi.QuoteAdded += _mtapi_QuoteAdded;
+            _mtapi.QuoteRemoved += _mtapi_QuoteRemoved;
+            _mtapi.QuoteUpdate += _mtapi_QuoteUpdate;
+
+            _mtapi.BeginConnect(8228);
+            _connnectionWaiter.WaitOne();
+
+            Console.WriteLine("Application finished. Press any key...");
+        }
+
+        static void _mtapi_ConnectionStateChanged(object sender, Mt5ConnectionEventArgs e)
+        {
+            switch (e.Status)
+            {
+                case Mt5ConnectionState.Connecting:
+                    Console.WriteLine("Connnecting...");
+                    break;
+                case Mt5ConnectionState.Connected:
+                    Console.WriteLine("Connnected.");
+                    _connnectionWaiter.Set();
+                    break;
+                case Mt5ConnectionState.Disconnected:
+                    Console.WriteLine("Disconnected.");
+                    _connnectionWaiter.Set();
+                    _mtapi.BeginConnect(8228);
+                    _connnectionWaiter.WaitOne();
+                    break;
+                case Mt5ConnectionState.Failed:
+                    Console.WriteLine("Connection failed.");
+                    _connnectionWaiter.Set();
+                    _mtapi.BeginConnect(8228);
+                    _connnectionWaiter.WaitOne();
+                    break;
+            }
+        }
+
+        static void _mtapi_QuoteAdded(object sender, Mt5QuoteEventArgs e)
+        {
+            Console.WriteLine("Quote added with symbol {0}", e.Quote.Instrument);
+        }
+
+        static void _mtapi_QuoteRemoved(object sender, Mt5QuoteEventArgs e)
+        {
+            Console.WriteLine("Quote removed with symbol {0}", e.Quote.Instrument);
+        }
+
+        static void _mtapi_QuoteUpdate(object sender, Mt5QuoteEventArgs e)
+        {
+            Console.WriteLine("Quote updated: {0} - {1} : {2}", e.Quote.Instrument, e.Quote.Bid, e.Quote.Ask);
+        }
+    }
+}
